a52d8c23c2099d5d8c7ae00e4e54d45a3b619836
Rahul saini <rs7965498@gmail.com>
2023-11-02 13:51:18 +0530

    Grade

--- GradingStudents.js
+++ GradingStudents.js
@@ -1,10 +1,11 @@
 /* HackerLand University has the following grading policy:
 
-Every student receives a  in the inclusive range from 0 to 100.
-Any  less than 40 is a failing grade.
-Sam is a professor at the university and likes to round each student's  according to these rules:
+Every student receives a in the inclusive range from 0 to 100.
+Any less than 40 is a failing grade.
 
-If the difference between the  and the next multiple of 5 is less than 3, round  up to the next multiple of 5.
+Sam is a professor at the university and likes to round each student's according to these rules:
+
+If the difference between the and the next multiple of 5 is less than 3, round  up to the next multiple of 5.
 If the value of  is less than 38, no rounding occurs as the result will still be a failing grade. */
 
 
